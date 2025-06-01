139b9f78bfc5fbd09809a7039ef74938bd248be3
kongali1720 <121486486+kongali1720@users.noreply.github.com>
2025-06-01 18:35:05 +0700

    Create brute-checker.py

---
+++ brute-checker.py
@@ -0,0 +1,21 @@
+<h1 align="center">💣🔐 brute-checker: Simulasi Brute Force Password - Edukatif & Aman</h1>
+
+<p align="center">
+  <b>Belajar bagaimana brute force bekerja secara aman tanpa meretas apa pun.</b><br>
+  <sub>100% Offline - Cocok buat edukasi & pemahaman dasar keamanan password</sub>
+</p>
+
+---
+
+## 🚀 Fitur
+- Mencontohkan metode brute force dasar
+- Dapat digunakan untuk edukasi di lab atau kelas
+- Offline, tidak mengakses situs/web manapun
+- Menyadarkan pentingnya penggunaan password yang kuat
+
+---
+
+## 🛠️ Cara Jalankan
+
+```bash
+python3 brute_checker.py
